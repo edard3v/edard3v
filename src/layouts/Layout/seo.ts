f1c41342cfcb817f39150dc23469f48327691926
edarcode <edarcode@gmail.com>
2024-12-01 00:58:27 -0500

    refactor

--- src/layouts/Layout/seo.ts
+++ src/layouts/Layout/seo.ts
@@ -5,5 +5,5 @@ export const SEO = {
   author: "edarcode",
   img: "https://console.cloudinary.com/pm/c-1c47c24a9b5be780a824370026cd4c/media-explorer/edarcode?assetId=c96c5c3f5baf178fd4a4114fd7835392",
   keywords:
-    "edarcode, desarrollador web, portafolio web, diseño web, programación, desarrollo front-end, desarrollo back-end, proyectos web, JavaScript, React, TypeScript, aplicaciones web, desarrollo de software, portafolio de desarrollo",
+    "edarcode, desarrollador web, portafolio web, diseño web, programación, desarrollo front-end, desarrollo back-end, proyectos web, JavaScript, React, Astro, TypeScript, aplicaciones web, desarrollo de software, portafolio de desarrollo",
 };
