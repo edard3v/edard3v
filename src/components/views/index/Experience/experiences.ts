4b0bc63c37e4efe1befb824724bd61c4cc49fcd0
edard3v <edard3v@gmail.com>
2024-12-08 21:49:30 -0500

    iniciando eperience

---
+++ src/components/views/index/Experience/experiences.ts
@@ -0,0 +1,8 @@
+export const EXPERIENCES = [
+  {
+    title: "Krekeet",
+    date: "2024, 15 días",
+    content:
+      "Crear vistas exponiendo información relevante destinada a la organización.",
+  },
+];
