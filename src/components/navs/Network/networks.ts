feb31fe489b04640d900b42ee190c8782ce5eb44
edard3v <edard3v@gmail.com>
2024-12-09 09:37:23 -0500

    agg iconos

---
+++ src/components/navs/Network/networks.ts
@@ -0,0 +1,33 @@
+import Discord from "../../icons/Discord.astro";
+import Github from "../../icons/Github.astro";
+import Instagram from "../../icons/Instagram.astro";
+import Linkedin from "../../icons/Linkedin.astro";
+import Youtube from "../../icons/Youtube.astro";
+
+export const NETWORKS = [
+  {
+    display: "github",
+    url: "https://github.com/edarcode",
+    Icon: Github,
+  },
+  {
+    display: "youtube",
+    url: "https://youtube.com/@edarcode8124?si=rdyoVhSE8RuFOy1r",
+    Icon: Youtube,
+  },
+  {
+    display: "instagram",
+    url: "https://www.instagram.com/edarcode",
+    Icon: Instagram,
+  },
+  {
+    display: "linkedin",
+    url: "https://www.linkedin.com/in/edarcode",
+    Icon: Linkedin,
+  },
+  {
+    display: "discord",
+    url: "https://discord.gg/HUfnRPjBHm",
+    Icon: Discord,
+  },
+];
