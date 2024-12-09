a13e4d877472d9a0d72f0afb931f1e89102a7943
edard3v <edard3v@gmail.com>
2024-12-09 10:17:05 -0500

    agg compo skills, pero sin usar

---
+++ src/components/views/index/Skills/skills.ts
@@ -0,0 +1,86 @@
+import AstroF from "../../../icons/AstroF.astro";
+import CSS from "../../../icons/CSS.astro";
+import Express from "../../../icons/Express.astro";
+import Git from "../../../icons/Git.astro";
+import Github from "../../../icons/Github.astro";
+import HTML from "../../../icons/HTML.astro";
+import JS from "../../../icons/JS.astro";
+import Node from "../../../icons/Node.astro";
+import Postgres from "../../../icons/Postgres.astro";
+import Prisma from "../../../icons/Prisma.astro";
+import React from "../../../icons/React.astro";
+import SQLite from "../../../icons/SQLite.astro";
+import TS from "../../../icons/TS.astro";
+
+export const SKILLS = [
+  {
+    name: "HTML",
+    Icon: HTML,
+    doc: "https://lenguajehtml.com/",
+  },
+  {
+    name: "CSS",
+    Icon: CSS,
+    doc: "https://lenguajecss.com/",
+  },
+  {
+    name: "JavaScript",
+    Icon: JS,
+    doc: "https://lenguajejs.com/",
+  },
+  {
+    name: "Git",
+    Icon: Git,
+    doc: "https://git-scm.com/doc",
+  },
+  {
+    name: "Github",
+    Icon: Github,
+    doc: "https://github.com/",
+  },
+  {
+    name: "TypeScript",
+    Icon: TS,
+    doc: "https://www.typescriptlang.org/",
+  },
+  {
+    name: "React",
+    Icon: React,
+    doc: "https://es.react.dev/learn",
+  },
+  {
+    name: "Astro",
+    Icon: AstroF,
+    doc: "https://docs.astro.build/es/getting-started",
+  },
+  {
+    name: "Node.js",
+    Icon: Node,
+    doc: "https://nodejs.org/docs/latest/api/",
+  },
+  {
+    name: "Express",
+    Icon: Express,
+    doc: "https://expressjs.com/es/",
+  },
+  {
+    name: "Prisma",
+    Icon: Prisma,
+    doc: "https://www.prisma.io/docs/orm",
+  },
+  // {
+  //   name: "DrizzleORM",
+  //   Icon: DrizzleORM,
+  //   doc: "https://orm.drizzle.team/docs/overview",
+  // },
+  {
+    name: "PostgreSQL",
+    Icon: Postgres,
+    doc: "https://www.postgresql.org/docs/",
+  },
+  {
+    name: "SQLite",
+    Icon: SQLite,
+    doc: "https://www.sqlite.org/docs.html",
+  },
+];
