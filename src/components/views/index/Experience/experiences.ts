299a15c27c88e4e1b41ffefccddd2c64e3c4802a
edard3v <edard3v@gmail.com>
2024-12-09 03:41:31 -0500

    agg educación

--- src/components/views/index/Experience/experiences.ts
+++ src/components/views/index/Experience/experiences.ts
@@ -3,6 +3,12 @@ export const EXPERIENCES = [
     title: "Krekeet",
     date: "2024, 15 días",
     content:
-      "Crear vistas exponiendo información relevante destinada a la organización.",
+      "Crear vistas exponiendo información relevante destinada a la organización Krekeet.",
+  },
+  {
+    title: "Tutor",
+    date: "2022, 3 meses",
+    content:
+      "Guía a los estudiantes durante las primeras etapas del curso. Asistencia en la solución de ejercicios y promoción de la colaboración grupal a través del uso de pair programming.",
   },
 ];
