e51102f663af41a2f813989499f74b1c09ec238d
edard3v <edard3v@gmail.com>
2024-12-09 19:11:00 -0500

    agg tools

---
+++ src/components/views/Tools/tools.ts
@@ -0,0 +1,135 @@
+export const TOOLS = [
+  {
+    href: "https://discord.com/channels/@me",
+    display: "Discord",
+  },
+  {
+    href: "https://app.slack.com/client/T02KS88FB0E/C080U8J8GUQ",
+    display: "Slack",
+  },
+  {
+    href: "https://web.whatsapp.com/",
+    display: "Whatsapp",
+  },
+  {
+    href: "https://cursos.devtalles.com/collections",
+    display: "Devtalles (Cursos)",
+  },
+  {
+    href: "https://chatgpt.com/",
+    display: "chatGPT (IA)",
+  },
+  {
+    href: "https://docs.astro.build/es/getting-started/",
+    display: "Astro (doc)",
+  },
+  {
+    href: "https://excalidraw.com/",
+    display: "Excalidraw (Pizarra)",
+  },
+  {
+    href: "https://co.pinterest.com/search/pins/?q=paginas%20web&rs=typed",
+    display: "Pinterest (Diseño)",
+  },
+  {
+    href: "https://www.lapa.ninja/search/",
+    display: "Lapa (Diseño)",
+  },
+  {
+    href: "https://www.behance.net/search/projects/",
+    display: "Behance (Diseño)",
+  },
+  {
+    href: "https://land-book.com/",
+    display: "Landbook Landinds (Diseño)",
+  },
+  { href: "https://dribbble.com/search/", display: "Dribbble (Diseño)" },
+  {
+    href: "https://www.figma.com/community/design-inspirations/",
+    display: "Figma (Diseño)",
+  },
+  { href: "https://imgto.xyz/", display: "Imgto (Compresor)" },
+  {
+    href: "https://imagecompressor.11zon.com/es/image-compressor/",
+    display: "11zon (Compresor)",
+  },
+  { href: "https://shots.so/", display: "Shots (IMGS)" },
+  { href: "https://www.freepik.es/", display: "Freepik (IMGS)" },
+  { href: "https://alphacoders.com/", display: "Alphacoders (IMGS)" },
+  { href: "https://www.pexels.com/es-es/", display: "Pexel (IMGS)" },
+  { href: "https://publicapi.dev/", display: "APIS free" },
+  { href: "https://www.svgrepo.com/", display: "SVG grepo" },
+  { href: "https://svgl.app/", display: "SVG de @pheralb" },
+  { href: "https://console.cloudinary.com/", display: "Cloudinary" },
+  { href: "https://www.prisma.io/docs/orm", display: "Prisma ORM" },
+  { href: "https://manz.dev/", display: "manzdev" },
+  { href: "https://midu.dev/", display: "midudev" },
+  { href: "https://anchoreum.com/#es", display: "Tooltip en tags <a> (Juego)" },
+  {
+    href: "https://cursos.doriandesings.com/course/css-intermedio",
+    display: "CSS intermedio dorian",
+  },
+  { href: "https://pagespeed.web.dev", display: "Lighthouse" },
+  {
+    href: "https://cruxvis.withgoogle.com/#/",
+    display: "CrUX Vis (Lighthouse)",
+  },
+  { href: "https://www.drawdb.app/", display: "Diagramas db" },
+  { href: "https://drizzle.run/visualizer", display: "Diagramas db drizzle" },
+  {
+    href: "https://number-conversion-tau.vercel.app/",
+    display: "Conversor de números (edarcode)",
+  },
+  {
+    href: "https://calculator-ten-khaki-94.vercel.app/",
+    display: "Calculadora (edarcode)",
+  },
+  { href: "https://ideogram.ai/", display: "Gerador de img (IA)" },
+  {
+    href: "https://notaku.vercel.app/",
+    display: "Sitio web de animes (edarcode)",
+  },
+  { href: "https://www.photopea.com/", display: "Photopea (Editor de fotos)" },
+  {
+    href: "https://www.befunky.com/es/crear/editor-de-fotos/",
+    display: "Befunky (Editor de fotos)",
+  },
+  { href: "https://neumorphism.io", display: "Neumorphism" },
+  { href: "https://icon-sets.iconify.design/", display: "Iconify (ICONS)" },
+  { href: "https://jakearchibald.github.io/svgomg/", display: "Optimizar svg" },
+  { href: "https://xsgames.co/animatiss/", display: "Animatis (Animaciones)" },
+  { href: "https://animista.net/", display: "Animista (Animaciones)" },
+  { href: "https://www.cattocss.com/", display: "Cattocss (Animaciones)" },
+  {
+    href: "https://ianlunn.github.io/Hover/",
+    display: "Hovercss (Animaciones BTN)",
+  },
+  {
+    href: "https://sarthology.github.io/Animatopy/",
+    display: "Animatopy (Animaciones)",
+  },
+  {
+    href: "https://caniuse.com/",
+    display: "Canise (Soporte Navegadores)",
+  },
+  {
+    href: "https://scrollrevealjs.org/api/sync.html",
+    display: "Animaciones con scroll (ScrollReveal)",
+  },
+  {
+    href: "https://michalsnik.github.io/aos/",
+    display: "AOS (Animaciones con scroll)",
+  },
+  {
+    href: "https://gsap.com/",
+    display: "GSAP (Animaciones PRO)",
+  },
+  {
+    href: "https://www.npmjs.com/package/@justinribeiro/lite-youtube",
+    display: "lite-youtube (NPM)",
+  },
+  {
+    href: "https://splidejs.com/guides/options/",
+    display: "Splide (Carruseles)",
+  },
+];
