a0fb14c740b2895cb029e255a58da305c14d6d23
edard3v <edard3v@gmail.com>
2024-12-10 18:10:48 -0500

    mejorando seo

--- src/layouts/Layout/seo.ts
+++ src/layouts/Layout/seo.ts
@@ -3,7 +3,7 @@ export const SEO = {
   description:
     "Soy un profesional dedicado al desarrollo de sitios web, enfocado en la calidad y el compromiso con cada proyecto.",
   author: "edard3v",
-  img: "https://console.cloudinary.com/pm/c-1c47c24a9b5be780a824370026cd4c/media-explorer/edarcode?assetId=c96c5c3f5baf178fd4a4114fd7835392",
+  img: "https://asset.cloudinary.com/dvmsllkkg/89a1542f9e78806d5a32957915fc2cc4",
   keywords:
     "edard3v, desarrollador web, portafolio web, diseño web, programación, desarrollo front-end, desarrollo back-end, proyectos web, JavaScript, React, Astro, TypeScript, aplicaciones web, desarrollo de software, portafolio de desarrollo",
 };
