21a4c44ca2f0b1c77b549ea9c62513b3660e148a
edard3v <edard3v@gmail.com>
2024-12-11 20:41:26 -0500

    agg animaciones a cards

--- src/utils/view.ts
+++ src/utils/view.ts
@@ -1,4 +1,4 @@
-export const view = (element: HTMLElement, cls: string) => {
+export const view = (element: HTMLElement, cls: string, threshold = 0.5) => {
   if (!element) return;
 
   // Define la función de callback para el IntersectionObserver
@@ -6,7 +6,10 @@ export const view = (element: HTMLElement, cls: string) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
         // Agrega la clase cuando el elemento está visible
+
         entry.target.classList.add(cls);
+        // setTimeout(() => {
+        // }, 500);
       } else {
         // Quita la clase cuando el elemento ya no está visible
         entry.target.classList.remove(cls);
@@ -17,7 +20,7 @@ export const view = (element: HTMLElement, cls: string) => {
   // Crea el IntersectionObserver
   const observer = new IntersectionObserver(observerCallback, {
     root: null, // Usa el viewport como contenedor de referencia
-    threshold: 0.5, // El 50% del elemento debe ser visible para que se active
+    threshold, // El 50% del elemento debe ser visible para que se active
   });
 
   // Observa el elemento seleccionado
