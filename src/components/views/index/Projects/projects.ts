9c5ecd58598caa3b013616ededebf57b38245e76
edard3v <edard3v@gmail.com>
2025-03-19 07:10:12 -0500

    agg project+

--- src/components/views/index/Projects/projects.ts
+++ src/components/views/index/Projects/projects.ts
@@ -1,18 +1,32 @@
 import dragon from "./imgs/dragon.avif";
 import krekeet from "./imgs/krekeet.avif";
 import coffee from "./imgs/coffee.avif";
+import notaku from "./imgs/notaku.avif";
 
 export const PROJECTS = [
   {
-    title: "Krekeet",
-    deploy: "https://krekeet.vercel.app/",
+    title: "Notaku",
+    deploy: "https://notaku-liart.vercel.app/",
     description:
-      "Agencia de comunicación digital especializada en diseño de experiencias de usuario.",
+      "Explora el mejor catálogo de animes online. Sinopsis, trailers, recomendaciones y más",
+    tools: ["Astro", "React"],
+    imgs: {
+      mobile: "",
+      laptop: "",
+      full: notaku,
+    },
+  },
+
+  {
+    title: "Coffee",
+    deploy: "https://coffee-aey.pages.dev/",
+    description:
+      "Sumérgete en el mundo del café. Descubre variedades únicas, aprende sobre su preparación y disfruta de la mejor experiencia cafetera, desde el grano hasta la taza.",
     tools: ["Astro"],
     imgs: {
       mobile: "",
       laptop: "",
-      full: krekeet,
+      full: coffee,
     },
   },
 
@@ -30,15 +44,15 @@ export const PROJECTS = [
   },
 
   {
-    title: "Coffee",
-    deploy: "https://coffee-aey.pages.dev/",
+    title: "Krekeet",
+    deploy: "https://krekeet.vercel.app/",
     description:
-      "Sumérgete en el mundo del café. Descubre variedades únicas, aprende sobre su preparación y disfruta de la mejor experiencia cafetera, desde el grano hasta la taza.",
+      "Agencia de comunicación digital especializada en diseño de experiencias de usuario.",
     tools: ["Astro"],
     imgs: {
       mobile: "",
       laptop: "",
-      full: coffee,
+      full: krekeet,
     },
   },
 ] as const;
