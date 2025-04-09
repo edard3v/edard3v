765d6cea192ff02446a61e0f088c8113f248d34e
edard3v <edard3v@gmail.com>
2025-04-09 03:06:56 -0500

    refactor

--- src/views/index/Experience/experiences.ts
+++ src/views/index/Experience/experiences.ts
@@ -3,7 +3,7 @@ export const EXPERIENCES = [
     title: "Joyful Pets",
     date: "2025, 2 meses",
     content:
-      "Desarrollo fullstack de tienda online de placas para perros en Costa Rica, con panel de administración incluido.",
+      "Desarrollo fullstack de tienda online de placas para perros en Costa Rica, con panel de administración.",
   },
   {
     title: "Krekeet",
