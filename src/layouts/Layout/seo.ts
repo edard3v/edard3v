26330f70195b41d99d062e863a1b902852540e80
edard3v <edard3v@gmail.com>
2026-05-19 14:04:24 -0500

    cambio img SEO

--- src/layouts/Layout/seo.ts
+++ src/layouts/Layout/seo.ts
@@ -3,7 +3,7 @@ export const SEO = {
   description:
     "Soy un profesional dedicado al desarrollo de sitios web, enfocado en la calidad y el compromiso con cada proyecto.",
   author: "edard3v",
-  img: "https://res.cloudinary.com/dzuo8j1qg/image/upload/v1733991158/edard3v.jpg",
+  img: "https://res.cloudinary.com/dzuo8j1qg/image/upload/v1779217110/edar_vgbl0i.jpg",
   keywords:
     "edard3v, edar, desarrollador web, portafolio web, diseño web, programación, desarrollo front-end, desarrollo back-end, proyectos web, JavaScript, React, Astro, TypeScript, aplicaciones web, desarrollo de software, portafolio de desarrollo",
 };
